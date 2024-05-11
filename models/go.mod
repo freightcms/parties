a1a64307ac4917cd8514df200d54c379034af192
Christopher Schoeder <christopher.schoeder@protonmail.com>
2024-05-10 20:51:41 -0500

    Update parties

--- models/go.mod
+++ models/go.mod
@@ -1,8 +1,8 @@
-module github.com/freightcms/partis/models
+module github.com/freightcms/parties/models
 
 go 1.22.1
 
 require (
-	github.com/freightcms/common/models v1.1.0 // indirect
-	github.com/freightcms/locations/models v1.0.0 // indirect
+	github.com/freightcms/common/models v1.1.0
+	github.com/freightcms/locations/models v1.0.0
 )
--- models/party.go
+++ models/party.go
@@ -6,22 +6,22 @@ import (
 )
 
 type Party struct {
-	RollupId        *string                     `json:"rollup_id,omitempty" bson:"rollup_id,omitempty"`
-	PhysicalAddress locationModels.AddressModel `json:"physical_address,omitempty" bson:"physical_address,omitempty"`
-	MailingAddress  locationModels.AddressModel `json:"mailing_address,omitempty" bson:"mailing_address,omitempty"`
+	RollupId        *string                     `json:"RollupId" bson:"RollupId"`
+	PhysicalAddress locationModels.AddressModel `json:"PhysicalAddress" bson:"PhysicalAddress"`
+	MailingAddress  locationModels.AddressModel `json:"MailingAddress" bson:"MailingAddress"`
 	commonModels.Contact
 }
 
 type Person struct {
 	Party
-	FirstName string `json:"first_name,omitempty" bson:"first_name,omitempty"`
-	LastName  string `json:"last_name,omitempty" bson:"last_name,omitempty"`
-	Title     string `json:"title,omitempty" bson:"title,omitempty"`
+	FirstName string `json:"FirstName" bson:"FirstName"`
+	LastName  string `json:"LastName" bson:"LastName"`
+	Title     string `json:"Title" bson:"Title"`
 }
 
 type Company struct {
 	Party
-	TaxId    *string                `json:"tax_id,omitempty" bson:"tax_id,omitempty"`
-	DBA      string                 `json:"dba,omitempty" bson:"dba,omitempty"`
-	Contacts []commonModels.Contact `json:"contacts,omitempty" bson:"contacts,omitempty"`
+	TaxId    string                 `json:"TaxId" bson:"TaxId"`
+	DBA      string                 `json:"DBA" bson:"DBA"`
+	Contacts []commonModels.Contact `json:"Contacts" bson:"Contacts"`
 }
